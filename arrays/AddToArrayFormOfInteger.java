a74128bc7c63845267ae18d94a31fe0b3d81bd4f
sarthaksinghaniya <sarthaksinghaniya789@gmail.com>
2026-01-05 13:55:47 +0530

    array dsa

---
+++ arrays/AddToArrayFormOfInteger.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        List<Integer> ans = new ArrayList<>();
+
+        int p = num.length - 1;
+        int carry = 0;
+
+        while (p >= 0 || k > 0 || carry > 0) {
+            int numval = (p >= 0) ? num[p] : 0;
+            int d = k % 10;
+
+            int sum = numval + d + carry;
+            ans.add(sum % 10);
+            carry = sum / 10;
+
+            p--;
+            k /= 10;
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+}
---
+++ arrays/TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> seen = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (seen.containsKey(complement)) {
+                return new int[] { seen.get(complement), i };
+            }
+            seen.put(nums[i], i);
+        }
+        // Return empty array if no solution found (LeetCode guarantees one solution though)
+        return new int[] {};
+    }
+}
