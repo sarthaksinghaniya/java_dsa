a446e76b6a6f3e138f2644acd2a2efd81251289c
sarthaksinghaniya <sarthaksinghaniya789@gmail.com>
2026-01-05 13:57:55 +0530

    dynamic q with java dsa

---
+++ dynamic_programming/NthTribonacciNumber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0){
+            return 0 ;
+        }else if(n==1 || n==2){
+            return 1 ;
+        }else{
+            int ft = 0;
+            int st = 1;
+            int tt = 1;
+
+            for(int i = 1 ; i<= n ; i ++){
+                int frt = ft + st + tt;
+                ft = st ;
+                st = tt ;
+                tt = frt ;
+
+            }
+            return ft ;
+        }
+    }
+}
