89a3bbfa9cefd6f006585c0bcc872bb5c5a65af0
sarthaksinghaniya <sarthaksinghaniya789@gmail.com>
2026-01-07 14:39:59 +0530

    done with leetcode 268 missing number

---
+++ arrays/MissingNumber.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int range = nums.length ;
+
+        int as = (range*(range + 1))/2 ; //n(n+1)/2
+
+        //actual sum h jb missing number present h 
+
+        int cs  = 0 ;
+
+        for(int i = 0 ; i < nums.length ; i++){
+            cs = cs + nums[i] ;
+
+        }
+         
+         int ans = as - cs ; 
+         return ans ;
+    }
+
+}
