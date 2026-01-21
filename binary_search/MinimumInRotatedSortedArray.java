6bd153cc41ea2f370414bd72263d9d4260a2c5fe
sarthaksinghaniya <sarthaksinghaniya789@gmail.com>
2026-01-21 18:31:12 +0530

    leet code problem 153 binary search minimum in rotated sorted array

--- binary_search/MinimumInRotatedSortedArray.java
+++ binary_search/MinimumInRotatedSortedArray.java
@@ -1,9 +1,10 @@
+// Leetcode 153. Minimum in Rotated Sorted Array
 class Solution {
-    public int findMin(int[] nums) {
+    public int findMin(int[] nums) { 
         if(nums.length == 1){
             return nums[0] ;
 
-        }else if(nums[0]<nums[nums.length-1]){
+        }else if(nums[0]<nums[nums.length-1]){ 
             return nums[0] ;
         }
         int st = 0 ;
@@ -16,16 +17,16 @@ class Solution {
                 return nums[mid] ; 
 
             }else if(mid!=nums.length - 1 && nums[mid]>nums[mid+1]){
-                return nums[mid+1] ;
+                return nums[mid+1] ; 
             }else if(nums[st]<=nums[mid]){ // left part is sorted 
-                st = mid + 1 ;
+                st = mid + 1 ; // move to right side
 
-            }else{ // right is wsorted
-                end = mid - 1 ;
+            }else{ // right is sorted
+                end = mid - 1 ; // move to left side
 
             }
         }
 
-        return -1 ;
+        return -1 ; // default return value
     }
 }
