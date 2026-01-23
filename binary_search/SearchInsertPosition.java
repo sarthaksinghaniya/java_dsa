fd92b67e3728a699d7997754a6f60b4626428074
sarthaksinghaniya <sarthaksinghaniya789@gmail.com>
2026-01-23 18:34:45 +0530

    leet code 35 an easy but yet important question

---
+++ binary_search/SearchInsertPosition.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int st = 0 ;
+        int end = nums.length - 1 ;
+        while (st<= end ){
+            int mid = (st + end)/2 ; 
+            if(nums[mid] == target){
+                return mid ; // found target
+
+            }else if(target > nums[mid]){
+                st = mid + 1 ;
+
+            }else{
+                end = mid - 1; 
+
+            }
+        }
+
+        return st ; // target not found, return insert position
+    }
+}
