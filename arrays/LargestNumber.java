8e5718672d61fef6f2f858c45661991620a86f82
sarthaksinghaniya <sarthaksinghaniya789@gmail.com>
2026-02-04 21:52:09 +0530

    gave some comment into largest number

--- arrays/LargestNumber.java
+++ arrays/LargestNumber.java
@@ -2,16 +2,16 @@
 import java.util.Arrays;
 class Solution {
     public String largestNumber(int[] nums) {
-        String[] arr = new String[nums.length];
+        String[] arr = new String[nums.length]; // convert int array to String array
 
         for(int i = 0 ; i < nums.length ; i++){
-            arr[i] = nums[i] + "" ;
+            arr[i] = nums[i] + "" ; // convert each integer to string
 
         }
 
-        Arrays.sort(arr,(s1,s2) -> (s1+s2).compareTo(s2+s1));
+        Arrays.sort(arr,(s1,s2) -> (s1+s2).compareTo(s2+s1)); // custom comparator to sort based on concatenated values
 
-        StringBuilder sb = new StringBuilder("") ;
+        StringBuilder sb = new StringBuilder("") ; // to build the final largest number string
 
         for(int i = arr.length-1 ; i>= 0 ; i--){
             sb.append(arr[i]) ;
